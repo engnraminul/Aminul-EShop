305941ef84259b60ce803e6cf4d6786b65330f4c
engnraminul <engnraminul@gmail.com>
2022-02-23 23:06:39 +0600

    create profile update form

--- Login/urls.py
+++ Login/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('registration/', views.registration, name='registration'),
     path('signin/', views.signin, name='signin'),
     path('signout/', views.signout, name='signout'),
+    path('profile/', views.profile, name='profile')
 ]
--- Login/views.py
+++ Login/views.py
@@ -7,6 +7,9 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, logout, authenticate
 
+#Message Module
+from django.contrib import messages
+
 # Model & Form
 from Login.models import Profile
 from Login.forms import ProfileForm, RegistrationForm
@@ -18,6 +21,7 @@ def registration(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "Your account create successfully")
             return HttpResponseRedirect(reverse('Login:signin'))
     return render(request, 'Login/registration.html', context={'form':form})
 
@@ -38,4 +42,18 @@ def signin(request):
 @login_required
 def signout(request):
     logout(request)
+    messages.warning(request, "You are logout successfully!")
     return HttpResponse("Signout User")
+
+@login_required
+def profile(request):
+    profile = Profile.objects.get(user=request.user)
+
+    form = ProfileForm(instance=profile)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile Update successfully")
+            form = ProfileForm(instance=profile)
+    return render(request, 'Login/update_profile.html', context={'form':form})
