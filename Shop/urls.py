20f7b6d580d84a5fb4300deb7d6f21cbf167b915
engnraminul <engnraminul@gmail.com>
2022-02-28 22:57:32 +0600

    Product detail view and url create

--- Shop/urls.py
+++ Shop/urls.py
@@ -4,4 +4,5 @@ app_name = 'Shop'
 
 urlpatterns = [
     path('', views.Product_List.as_view(), name='home'),
+    path('detail/<pk>/', views.ProductDetail.as_view(), name='detail')
 ]
--- Shop/views.py
+++ Shop/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 # Create your views here.
 from Shop.models import Product
+#LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class Product_List(ListView):
     model = Product
     template_name = 'Shop/home.html'
+
+
+class ProductDetail(LoginRequiredMixin, DetailView):
+    model = Product
+    template_name = 'shop/product_detail.html'
