8b550e06067a131b36ad729c0db2425a4e08d7a8
engnraminul <engnraminul@gmail.com>
2022-03-12 19:39:53 +0600

    Start SSl commerz system

--- Payment/views.py
+++ Payment/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, redirect
 from Order.models import CartToOrder
 from Payment.models import ShippingAddress
 from Payment.forms import ShippingForm
@@ -6,6 +6,12 @@ from Payment.forms import ShippingForm
 from django.contrib.auth.decorators import login_required
 
 from django.contrib import messages
+
+#payment
+import requests
+#from sslcommerz_python.payment import SSLCSession
+from decimal import Decimal
+import socket
 
 @login_required
 def checkout(request):
@@ -24,3 +30,17 @@ def checkout(request):
     order_total = order_qs[0].get_totals()
 
     return render(request, 'Payment/checkout.html', context={"form":form, "order_items":order_items, "order_total":order_total, "saved_address":saved_address})
+
+
+@login_required
+def payment(request):
+    saved_address = ShippingAddress.objects.get_or_create(user=request.user)
+    saved_address = saved_address[0]
+    if not saved_address.is_fully_filled():
+        messages.info(request, f"please complete Shipping address form!")
+        return redirect("Payment:checkout")
+
+    if not request.user.profile.is_fully_fillup():
+        messages.info(request, f"please complete your profile details!")
+        return redirect("Login:profile")
+    return render(request, "Payment/payment.html")
