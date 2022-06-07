2416ed19f4ca6ac0ca0721861134890b607a79ef
engnraminul <engnraminul@gmail.com>
2022-06-07 18:39:46 +0600

    Admin pannel add list display, search, and clone object

--- Shop/admin.py
+++ Shop/admin.py
@@ -3,5 +3,23 @@ from Shop.models import Category, Product
 
 # Register your models here.
 
+def duplicate_event(modeladmin, request, queryset):
+        for object in queryset:
+            object.id = None
+            object.save()
+duplicate_event.short_description = "Clone"
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('Product_name', 'Category', 'created_date', 'price')
+    list_filter = ('Category',)
+    search_fields = ('Product_name',)
+    actions = [duplicate_event]
+
+    
+
+
+
+
+
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
